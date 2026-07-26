af6163dd4fdc18daed079b9542240531ac5d5e76
Adriano Costa <adrianojesuscosta@gmail.com>
2026-07-26 13:38:01 -0300

    FIX: Criando middleware.py para resolver erro de importação

--- apps/api/middleware.py
+++ apps/api/middleware.py
@@ -1,14 +1,10 @@
-# api/middleware.py
-import logging
-
-logger = logging.getLogger(__name__)
+# apps/api/middleware.py
+# Middleware vazio para evitar erro de importação
 
 class OriginLogMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        origin = request.META.get("HTTP_ORIGIN")
-        logger.info(f"Requisição recebida: {request.method} {request.path} | Origin: {origin}")
         response = self.get_response(request)
         return response
