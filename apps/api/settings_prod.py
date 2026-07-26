1ce3b38c1bb3e0f43bf346289a8f17cfd29e55e5
Adriano Costa <adrianojesuscosta@gmail.com>
2026-07-26 13:18:00 -0300

    FIX: Corrigindo STATIC_ROOT para o local correto

--- apps/api/settings_prod.py
+++ apps/api/settings_prod.py
@@ -204,7 +204,7 @@ CORS_ALLOW_HEADERS = [
 STATIC_URL = '/static/'
 
 # 🔥 CORRIGIDO: Usando caminho absoluto para evitar problemas
-STATIC_ROOT = '/opt/render/project/src/staticfiles'
+STATIC_ROOT = '/opt/render/project/src/apps/api/api/staticfiles'
 # Isso vai para: /opt/render/project/src/apps/staticfiles
 
 # Se tiver arquivos estáticos próprios
