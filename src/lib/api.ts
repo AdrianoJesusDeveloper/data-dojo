a0cefa6ec765c01baf60272b1a20d10d9ae58a28
Adriano Costa <adrianojesuscosta@gmail.com>
2026-07-27 20:05:36 -0300

    debug: mostra API_URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
+console.log("VITE_API_URL =", API_URL);
+
 export const api = axios.create({
   baseURL: API_URL,
   headers: {
